10287f58a731eef711c03fd8afee01b61c64cff7
bdetchison <bdetchison@gmail.com>
2016-07-07 21:35:41 -0400

    Add Push Notifications

--- VibrationMachineLearningService/VibrationMachineLearningService/Areas/AzureMachineLearning/Controllers/ProcessVIbrationDataController.cs
+++ VibrationMachineLearningService/VibrationMachineLearningService/Areas/AzureMachineLearning/Controllers/ProcessVIbrationDataController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Azure.NotificationHubs;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
@@ -7,6 +8,8 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Script.Serialization;
 using VibrationMachineLearningService.Areas.AzureMachineLearning.Models;
+using System.Threading.Tasks;
+using System.Web;
 
 namespace VibrationMachineLearningService.Areas.AzureMachineLearning.Controllers
 {
@@ -31,6 +34,42 @@ namespace VibrationMachineLearningService.Areas.AzureMachineLearning.Controllers
             MachineLearningRequest.InvokeRequestResponseService(ProcessMachineLearningResult, velocityReadings);
         }
 
+
+        private async void SendPushNotification(string message)
+        {
+            Microsoft.Azure.NotificationHubs.NotificationOutcome outcome = null;
+            HttpStatusCode ret = HttpStatusCode.InternalServerError;
+
+            // Windows 8.1 / Windows Phone 8.1
+            var toast = @"<toast><visual><binding template=""ToastText01""><text id=""1"">" +
+                        message + "</text></binding></visual></toast>";
+
+            
+            outcome = await Notifications.Instance.Hub.SendWindowsNativeNotificationAsync(toast);
+
+            if (outcome != null)
+            {
+                if (!((outcome.State == Microsoft.Azure.NotificationHubs.NotificationOutcomeState.Abandoned) ||
+                    (outcome.State == Microsoft.Azure.NotificationHubs.NotificationOutcomeState.Unknown)))
+                {
+                    ret = HttpStatusCode.OK;
+                }
+            }
+        }
+
+        public class Notifications
+        {
+            public static Notifications Instance = new Notifications();
+
+            public NotificationHubClient Hub { get; set; }
+
+            private Notifications()
+            {
+                Hub = NotificationHubClient.CreateClientFromConnectionString("Endpoint=sb://vibrationprediction.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=QTDCRfLKVk1yYCY+ifJA7dbzq0Cj41p+7C7pMtO6UqM=",
+                                                                             "VibrationPredictionFailure");
+            }
+        }
+
         private void ProcessMachineLearningResult(string jsonObj, double lastDataReading)
         {
             if (jsonObj.Length > 0)
@@ -41,12 +80,13 @@ namespace VibrationMachineLearningService.Areas.AzureMachineLearning.Controllers
                 var predictedValue = responseObject.Results.output1.value.Values.First()[1];
 
                 dataAccess.InsertMachineLearningData(predictedValue, lastDataReading);
+                
 
                 if ( predictedValue> lastDataReading + .1 
                     || predictedValue == 0)//0 is listed if there is an outlier
                 {
-                    //TODO Add push notification
                     //failure coming
+                    SendPushNotification(string.Format("Failure Predicted:  Current actual reading:  {0}",lastDataReading));
                 }
             }
         }
